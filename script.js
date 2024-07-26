f2097eab898aebc15a10ed6708069ed9ad261d20
KC1064 <kiron08072003@gmail.com>
2024-07-26 23:02:51 +0530

    Added the functionalities of the app

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("expense-form");
+const list = document.getElementById("exp-list");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const desc = document.getElementById("description").value;
+  const catg = document.getElementById("category").value;
+  const amt = document.getElementById("amount").value;
+
+  if (desc && catg !== "Select Category" && !isNaN(amt) && amt !== "") {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `<td>${desc}</td>
+            <td>${catg}</td>
+            <td>${amt}</td>`;
+
+    list.appendChild(newRow);
+    document.getElementById("description").value = "";
+    document.getElementById("category").value = "Select Category";
+    document.getElementById("amount").value = "";
+  }
+});
